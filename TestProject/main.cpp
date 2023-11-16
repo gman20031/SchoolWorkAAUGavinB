9647424614aea297e4735e8f2d0e43966d1456bb
gman20031 <gavin.brandenburger1@gmail.com>
2023-11-16 02:25:34 -0800

    Quick example of static variables
    Need to do static member variables and functions

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
-#include <array>
+#include <stdlib.h>
 
-class myAss {
-public:
-	int *foo = nullptr;
-	int bar[3]{ 1,2,3 };
+void DoSomething();
+void DoStatic();
 
-	static void printArr(int * array) {
-		for (int i = 0; i < 3; ++i) {
-			std::cout << array[i] << ' ';
-		}
-		std::cout << std::endl;
+int main()
+{
+	//--------------------------------------------------------------------------------------------------------
+	// Quick example of scoping
+	// When you call a function, that function has its own scope
+	// so when the function is finished, everything withini that function is deleted and freed from memory
+	
+	// For example, in this do something function
+	// It initializes the number 3, adds four, then prints
+	for (int i = 0; i < 3; ++i)
+	{
+		DoSomething(); 
 	}
-};
+	// Since the variable number, is scoped to the function. every time the function completes it deletes itself
+	// Then the function re allocated and initalizes the variable to the integer 3
+	// adds the 2 and prints it
+	std::cout << std::endl;
+	// If you want to get around variables deleting themselves, you can use static
+	// Static tells the compiler to allocate the memory for this variable once
+	// and not delete it until the program finishes.
+	for (int i = 0; i < 3; ++i)
+	{
+		DoStatic();
+	}
+	// So because this is a statically allocated integer, it will not delete at function end
+	// and neither will it reinialize itself to 3, the creation is only done once at compile time.
+	// That causes this function to simply add 2 every time it is run and print that.
+}
 
-int main() {
-	myAss objectOne;
-	objectOne.bar[1] = 4;
-	objectOne.foo = (int*)malloc(sizeof(int) * 3);
-	objectOne.foo[0] = 1;
-	objectOne.foo[1] = 2;
-	objectOne.foo[2] = 3;
+void DoSomething()
+{
+	int number = 3;
+	number += 2;
+	std::cout << number << std::endl;
+}
 
-	myAss objectTwo = objectOne;
-	objectTwo.bar[2] = 4;
-	objectTwo.foo[2] = 4;
-
-	myAss::printArr(objectOne.bar);
-	myAss::printArr(objectTwo.bar);
-	myAss::printArr(objectOne.foo);
-	myAss::printArr(objectTwo.foo);
-
-
+void DoStatic()
+{
+	static int number = 3;
+	number += 2;
+	std::cout << number << std::endl;
 }
