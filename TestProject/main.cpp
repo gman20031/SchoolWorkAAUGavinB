435b767247a7fdb1529f95b282a5c986898a6b6b
gman20031 <gavin.brandenburger1@gmail.com>
2023-11-23 02:29:00 -0800

    Zipping and output

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
+#include <chrono>
 
-// In original C, the use of static for both functions and variables has a simple use
-// in addition to the simple use of static variables outlined in a different example file
-// static functions and variables are both unable to be called from outside of the same file
-// therefor if there is any capablility of someone gaining acess to your function while being in a different file
-// they would be unable to use that function
-// this is hard to make an example of, and to my current understanding is slightly redundant with the use of header files.
-
-// in C++ comes member functions and variables, which have different meanings to the normal static version
-class myClass {
-public:
-	int m_integer;
-	static int m_staticInteger;
-
-	void IntegerDoubleAdd(int input);
-	static void IntegerDoubleAdd(int input);
-
-};
+long fibonacci(unsigned n)
+{
+	std::cout << n << std::endl;
+	if (n < 2) return n;
+	return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 int main()
 {
+	for(;;)
+	{
+		int input;
+		std::cin >> input;
+		std::cout << "f(x) = " << fibonacci(input) << '\n';
+	}
 	
+	return 0;
+	//std::chrono::milliseconds mil(1000);
+	//mil = mil * 60;
 
-
+	//for (;;)
+	//{
+	//	if()
+	//}
 }
