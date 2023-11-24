127eca4ba3ce16b1688f688a64d89621e001cdb6
gman20031 <gavin.brandenburger1@gmail.com>
2023-11-24 01:50:09 -0800

    Looking Into SimC

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
 #include <chrono>
-
-long fibonacci(unsigned n)
-{
-	std::cout << n << std::endl;
-	if (n < 2) return n;
-	return fibonacci(n - 1) + fibonacci(n - 2);
-}
+#include <ratio>
 
 int main()
 {
-	for(;;)
-	{
-		int input;
-		std::cin >> input;
-		std::cout << "f(x) = " << fibonacci(input) << '\n';
-	}
-	
-	return 0;
-	//std::chrono::milliseconds mil(1000);
-	//mil = mil * 60;
+	constexpr double kMilisecondPerSecond = 1000;
+	constexpr double kNanosecondPerMilisecond = 1000 * 1000;	
 
-	//for (;;)
-	//{
-	//	if()
-	//}
+	std::chrono::time_point<std::chrono::steady_clock> startTime = std::chrono::steady_clock::now();
+	// takes the time since the clocks epoch, which is just some arbitrary number?
+
+	std::chrono::duration<double, std::milli> elapsed_miliseconds;
+	std::chrono::duration<double> elapsed_seconds;
+
+
+	std::chrono::time_point<std::chrono::steady_clock> currentTime; 
+
+	for (;;)
+	{
+		currentTime = std::chrono::steady_clock::now();
+		elapsed_miliseconds = (currentTime - startTime); // this somehow automatically converts the currentTime - startTime, which is in nanoseconds, to miliseconds
+															// this is probably exactly what a duration object is used for. since the standard
+		if (elapsed_miliseconds.count() > kMilisecondPerSecond * 2)
+		{
+			break;
+		}
+	}
+	elapsed_seconds = (currentTime - startTime);
+
+	std::cout << elapsed_seconds.count() << " - seconds\n";
+	std::cout << elapsed_miliseconds.count() << " - miliseconds\n";
+
+	return 0;
 }
