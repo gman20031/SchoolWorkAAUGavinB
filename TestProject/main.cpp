5a8161366329dc19ce129a3fa4129b6abaee9a71
gman20031 <gavin.brandenburger1@gmail.com>
2023-12-02 00:22:40 -0800

    including libraires makes no sense at all

--- TestProject/main.cpp
+++ TestProject/main.cpp
@@ -1,23 +1,4 @@
 #include <iostream>
-#include <chrono>
-#include <ratio>
-
-class MyClass {
-	int xVal;
-	int yVal;
-	bool foo;
-
-	void initialize()
-	{
-
-	}
-
-public:
-	MyClass(int x, int y, bool foolian)
-	{
-
-	};
-};
 
 int main()
 {
