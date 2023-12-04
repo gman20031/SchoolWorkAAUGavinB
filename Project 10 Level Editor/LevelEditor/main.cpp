9f04aab03b65599a38911cc4d8cb619210a9a300
gman20031 <gavin.brandenburger1@gmail.com>
2023-12-03 20:12:42 -0800

    Added SDL

--- Project 10 Level Editor/LevelEditor/main.cpp
+++ Project 10 Level Editor/LevelEditor/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+	SDL_Window* myWindow = SDL_CreateWindow("hi",0,0,50,50,0);
 
+	for (;;)
+	{
+
+	}
+	return 0;
 }
